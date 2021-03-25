1a0d00df0dc6dfcd4ecb5ac96a73684e3dd8cc88
Oleksandr <gostar1337@gmail.com>
2021-03-25 20:49:57 +0200

    update

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -45,7 +45,7 @@ class BookListContainer extends React.Component {
   }
 }
 
-const mapStateToProps = ({ books, loading, error }) => {
+const mapStateToProps = ({ bookList: { books, loading, error } }) => {
   return {
     books,
     loading,
